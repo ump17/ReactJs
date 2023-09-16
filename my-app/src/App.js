bebd76f7c1407453f507b369ef3e2e2e422bb54b
Ashish Singh <ashu63852@gmail.com>
2023-09-15 23:28:38 -0700

    saved initial changes

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="blank">
+      <a href='google.com' className="link">
+        Phishing website p enter kijiye
+      </a>
+    </div>
+  );
+}
+
+export default App;
