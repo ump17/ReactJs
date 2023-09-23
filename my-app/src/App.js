be5fe5786ad4a37bc970380fa1ba6fc29261b063
Ashish Singh <ashu63852@gmail.com>
2023-09-22 22:17:25 -0700

    api call added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import { useState } from 'react';
-
+import axios from "axios";
 
 function App() {
 
@@ -15,6 +15,14 @@ function App() {
     setMode('dark');
   }
 
+  const testingApiCall=()=>{
+    axios.get('https://jsonplaceholder.typicode.com/users').then((response)=>{
+      console.log(response);
+    }).catch((e)=>{
+      console.log(e);
+    })
+  }
+
 
   return (
    <>
@@ -22,6 +30,7 @@ function App() {
 
 <div className="container mt-5">
 <TextForm title="Enter text here: "></TextForm>
+<button className='btn btn-danger' onClick={testingApiCall} > Call Api</button>
 </div>
 
    </>
